fe66c3424323697e38a104d184ade85e0e7be778
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-08 13:42:07 +0200

    Remove deprecated Signal argument

--- froide_crowdfunding/__init__.py
+++ froide_crowdfunding/__init__.py
@@ -4,4 +4,4 @@ from django.dispatch import Signal
 
 default_app_config = "froide_crowdfunding.apps.FroideCrowdfundingConfig"
 
-contribution_successful = Signal(providing_args=["contribution"])
+contribution_successful = Signal()  # providing args ["contribution"]
